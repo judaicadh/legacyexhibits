3261336bb185752ec034b44172b85b6a268f7e1f
judaicadhpenn <157063908+judaicadhpenn@users.noreply.github.com>
2025-01-02 15:44:07 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://judaicadh.github.io',
+	base: 'legacyexhibits',
 	integrations: [
 		starlight({
 			title: 'Exhibits',
