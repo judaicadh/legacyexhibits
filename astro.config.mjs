ac2fb0ed7aab4cee454c5b2c2a405740c3b57892
judaicadhpenn <157063908+judaicadhpenn@users.noreply.github.com>
2025-03-31 17:05:17 -0400

    added images and styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,20 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import tailwind from '@astrojs/tailwind';
 import starlightImageZoom from 'starlight-image-zoom';
-// https://astro.build/config
+
 export default defineConfig({
 	site: 'https://judaicadh.github.io',
-	base: 'legacyexhibits',
+	base: '/legacyexhibits',
 	integrations: [
-		starlight({
-			plugins: [starlightImageZoom({
-				showCaptions: true,
-				}),
 
+		starlight({
+			customCss: ['./src/styles/tailwind.css',
+				'@fontsource-variable/playfair-display', '@fontsource-variable/Lora'],
+			plugins: [
+				starlightImageZoom({
+					showCaptions: true,
+				}),
 			],
 			title: 'Exhibits',
 			tableOfContents: { minHeadingLevel: 2, maxHeadingLevel: 5 },
@@ -19,18 +22,14 @@ export default defineConfig({
 				github: 'https://github.com/judaicadh/legacyexhibits',
 			},
 			sidebar: [
-				/*{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},*/
 				{
 					label: 'Exhibits',
 					autogenerate: { directory: 'reference' },
 				},
 			],
+		}),
+		tailwind({
+			applyBaseStyles: false, // Keeps Tailwind from overriding base styles
 		}),
 	],
 });
