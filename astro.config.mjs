1b3e358d98015daffc26616b02cd6ebb738d992f
judaicadhpenn <157063908+judaicadhpenn@users.noreply.github.com>
2025-08-21 19:53:58 -0400

    edited milgroim

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import netlify from '@astrojs/netlify';
 
 import tailwindcss from '@tailwindcss/vite';
 import starlightFullViewMode from "starlight-fullview-mode";
-import starlightDocSearch from '@astrojs/starlight-docsearch';
 
+import starlightScrollToTop from 'starlight-scroll-to-top'
 export default defineConfig({
   site: 'https://exhibits.judaicadhpenn.org',
 
@@ -20,12 +20,39 @@ export default defineConfig({
           plugins: [
               starlightImageZoom({showCaptions: true}),
               starlightFullViewMode({}),
-           
+              starlightScrollToTop(),
+
+
           ],
+          pagefind: {
+              ranking: {
+                  pageLength: 0.7,
+                  termFrequency: 1.0,
+                  termSaturation: 0.9,
+                  termSimilarity: 1.0,
+              },
+              mergeIndex: [
+                  {
+                      bundlePath: 'https://exhibits.judaicadhpenn.org/pagefind/',
+                      baseUrl: 'https://exhibits.judaicadhpenn.org',
+                      indexWeight: 1.0,
+                      mergeFilter: { site: 'Exhibits Website' },
+                      language: 'en'
+                  },
+                  {
+                      bundlePath: 'https://judaicadhpenn.org/pagefind/',
+                      baseUrl: 'https://judaicadhpenn.org',
+                      indexWeight: 0.8, // local content weighted highest
+                      mergeFilter: { site: 'Judaica Website' },
+                      language: 'en',
+                  },
+              ],
+          },
           title: 'Judaica Exhibitions',
           description: 'Online exhibitions from Judaica at Penn Libraries',
           tableOfContents: {minHeadingLevel: 2, maxHeadingLevel: 6},
           lastUpdated: true,
+          favicon: './assets/favicon.ico',
           logo: {src: './src/assets/pomegranate.svg'},
 
       }),
